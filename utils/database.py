1cd0385391a70a9e77676afabbbe8e71fc98090e
Rolando Urquiza <rolurquiza@gmail.com>
2019-01-17 10:29:06 -0500

    Allows to use uuid as id for documents in tinydb

--- utils/database.py
+++ utils/database.py
@@ -1,6 +1,36 @@
+import uuid
+
 from aiotinydb import AIOTinyDB
 from starlette.datastructures import DatabaseURL
 from starlette.types import ASGIApp, ASGIInstance, Scope
+from tinydb import Query, TinyDB
+from tinydb.database import Document, StorageProxy, Table
+
+Q = Query()
+
+
+class UuidTable(Table):
+    """
+    TinyDB table class to generate random uuid's as id's for documents
+    """
+
+    def _get_next_id(self) -> str:
+        return uuid.uuid4().hex
+
+
+class UuidStorageProxy(StorageProxy):
+    """
+    TinyDB storage proxy that allows string keys (uuid's) to be used
+    as document id's
+    """
+
+    def _new_document(self, key: str, value: dict) -> Document:
+        return Document(value, key)
+
+
+# set table and proxy on AIOTinyDB since that's what's being used
+AIOTinyDB.table_class = UuidTable
+AIOTinyDB.storage_proxy_class = UuidStorageProxy
 
 
 class TinyDbMiddleware:
