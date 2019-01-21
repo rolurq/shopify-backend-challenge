a2b09c54e47c6bb7cbaae03b36e6a475e81844c9
Rolando Urquiza <rolurquiza@gmail.com>
2019-01-20 19:15:20 -0500

    Fix models conversion to documents async calls

--- akara/models/mutations.py
+++ akara/models/mutations.py
@@ -9,6 +9,7 @@ from config.settings import SECRET_KEY, JWT_ALGORITHM, JWT_EXPIRATION_DAYS
 from .inputs import *
 from .types import *
 from ..utils.database import Q
+from ..utils.authentication import requires
 
 __all__ = ("Signup", "Login", "AddToCart", "RemoveFromCart")
 
@@ -37,7 +38,7 @@ class Signup(graphene.Mutation):
                 raise GraphQLError("username is alredy taken")
 
             user.set_password(input.password)
-            user.id = users.insert(user.to_doc())
+            user.id = users.insert(await user.to_doc())
 
         return Signup(
             token=jwt.encode(
@@ -69,7 +70,7 @@ class Login(graphene.Mutation):
             if not user:
                 raise GraphQLError("user does not exist")
 
-            user = User.from_doc(user)
+            user = await User.from_doc(user)
             if not user.check_password(password):
                 raise GraphQLError("incorrect password")
 
@@ -86,6 +87,14 @@ class Login(graphene.Mutation):
 
 
 class AddToCart(graphene.Mutation):
+    """
+    Mutation to add item to shopping cart
+
+    :param productId str: id of the product to add
+    :param amount int: amount of the product to add, defaults to 1
+    :return: the current state of the cart
+    """
+
     class Arguments:
         productId = graphene.ID(required=True)
         amount = graphene.Int(default_value=1)
@@ -93,8 +102,46 @@ class AddToCart(graphene.Mutation):
     Output = graphene.NonNull(Cart)
 
     @staticmethod
-    def mutate(root, info, productId: str, amount: int):
-        pass
+    @requires("authenticated", message="you must be logged in to access the cart")
+    async def mutate(root, info, productId: str, amount: int):
+        request = info.context["request"]
+        user = request.user
+
+        async with request.database as db:
+            products = db.table("products")
+            cart_items = db.table("cart_items")
+
+            product = products.get(doc_id=productId)
+            if not product:
+                raise GraphQLError("product does not exists")
+
+            product = await Product.from_doc(product)
+
+            # get the cart item, there must be only one, given that the convination of user
+            # and product is used both for queryng and storing
+            item = cart_items.get((Q.user == user.id) & (Q.product == productId))
+            current_order = 0
+            if item:
+                current_order = item.get("amount")
+
+            if product.inventory_count < amount + current_order:
+                raise GraphQLError(
+                    "there's not enougth availability to fullfil your order"
+                )
+
+            if not item:
+                item = CartItem(product=product, amount=amount)
+                item.user_id = user.id
+                cart_items.insert(await item.to_doc())
+            else:
+                item["amount"] += amount
+                cart_items.write_back([item])
+
+            # the cart is the list of cart items associated to a user
+            # there's not explicit model for the cart in the storage
+            cart = cart_items.search(Q.user == user.id)
+
+        return await Cart.from_doc(cart, request.database)
 
 
 class RemoveFromCart(graphene.Mutation):
